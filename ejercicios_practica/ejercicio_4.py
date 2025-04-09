2a913809177ccef73c511b0ed2978fd37a4d9251
Marrtiinnn <martin.estudios.ifz@gmail.com>
2025-04-09 15:52:52 -0300

    Actualizo ejercicio 4

--- ejercicios_practica/ejercicio_4.py
+++ ejercicios_practica/ejercicio_4.py
@@ -17,10 +17,10 @@ def grid():
     
     fig = plt.figure()
     fig.suptitle('Grilla con 4 funciones', fontsize=16)
-
-    #ax1 = fig.add_subplot(1, 2, 1)
-    #ax2 = fig.add_subplot(1, 2, 2)
-    #ax3 = fig.add_subplot(2, 2, 3)
+    # Aqui borre 3 # que estaban comentando las variables
+    ax1 = fig.add_subplot(1, 2, 1)
+    ax2 = fig.add_subplot(1, 2, 2)
+    ax3 = fig.add_subplot(2, 2, 3)
     ax4 = fig.add_subplot(3, 2, 1)
 
     ax1.plot(x, y1, color='darkred', label='y1 = x**2')
@@ -48,6 +48,9 @@ def grid():
     ax4.set_xlabel("X[rads]")
     ax4.legend()
     
+    plt.tight_layout()
+
+
     plt.show()
 
 if __name__ == '__main__':
@@ -91,6 +94,40 @@ if __name__ == '__main__':
     # Colocar una grilla a elección
 
     # Crear acá su gráfico
+
+    # Crear acá su gráfico
+
+    fig = plt.figure()
+    fig.suptitle('Grilla con 4 funciones', fontsize=16, color='blue')
+
+    ax1 = fig.add_subplot(2, 2, 1)
+    ax2 = fig.add_subplot(2, 2, 2)
+    ax3 = fig.add_subplot(2, 2, 3)
+    ax4 = fig.add_subplot(2, 2, 4)
+
+    ax1.plot(x, y1, color='red', label='y1 = x**2')
+    ax1.set_facecolor('whitesmoke')
+    ax1.grid(ls='dashdot')
+    ax1.legend()
+
+    ax2.plot(x, y2, color='green', label='y2 = x**3')
+    ax2.set_facecolor('whitesmoke')
+    ax2.grid(ls='solid')
+    ax2.legend()
+
+    ax3.plot(x, y3, color='blue', label='y3 = x**4')
+    ax3.set_facecolor('whitesmoke')
+    ax3.grid(ls='dotted')
+    ax3.legend()
+
+    ax4.plot(x, y4, color='purple', label='y4 = sqrt(x)')
+    ax4.set_facecolor('whitesmoke')
+    ax4.grid(ls='dashed')
+    ax4.legend()
+
+    plt.tight_layout()
+    plt.show()
+
 
     grid()
 
